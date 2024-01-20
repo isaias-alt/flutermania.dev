43ea94da054f310bb6932f24ba2d79d60cae8537
isaias-alt <cascolucasisaias@gmail.com>
2024-01-20 19:23:08 -0300

    feat: blog ended

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,23 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import { SITE_URL } from "./src/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: SITE_URL,
+  integrations: [
+    tailwind({
+      applyBaseStyles: true,
+    }),
+    sitemap(),
+    mdx(),
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: "one-dark-pro",
+    },
+  },
 });
---
+++ src/config.ts
@@ -0,0 +1,17 @@
+import type { Socials } from "./types";
+
+export const SITE_URL = "https://vervetheme.vercel.app"; // Replace with your site url
+export const TITLE = "Fluttermania";
+export const SITE_TITLE = "Fluttermania — A blog by Lucas Casco";
+export const SOCIAL_NAME = "Lucas Casco";
+export const SITE_DESCRIPTION = "A Great Flutter Blog";
+export const SITE_LOGO = "/logo.svg";
+
+export const SOCIALS: Socials = [
+  {
+    name: "Github",
+    href: "https://github.com/isaias-alt",
+    linkTitle: ` ${SOCIAL_NAME} on Github`,
+    active: true,
+  },
+];
