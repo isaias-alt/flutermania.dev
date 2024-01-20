a1fd1f89378bd73206fedd92a8adc818a1958619
isaias-alt <cascolucasisaias@gmail.com>
2024-01-20 19:24:24 -0300

    feat

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import type { Socials } from "./types";
 
-export const SITE_URL = "https://vervetheme.vercel.app"; // Replace with your site url
+export const SITE_URL = "";
 export const TITLE = "Fluttermania";
 export const SITE_TITLE = "Fluttermania — A blog by Lucas Casco";
 export const SOCIAL_NAME = "Lucas Casco";
